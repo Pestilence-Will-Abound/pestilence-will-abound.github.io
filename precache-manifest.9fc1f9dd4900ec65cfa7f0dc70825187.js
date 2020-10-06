05edc04a40d78934186ac484b37c7347e705c7b2
Colin Brayton <gringolalia@gmail.com>
2020-10-06 14:23:26 -0300

    deploy

---
+++ precache-manifest.9fc1f9dd4900ec65cfa7f0dc70825187.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f4222be06aada5eaf1",
+    "url": "/css/about.776d30f8.css"
+  },
+  {
+    "revision": "ddfd96833993224d990d",
+    "url": "/css/app.0cf09823.css"
+  },
+  {
+    "revision": "9dbc0a19f13ba1a795f7",
+    "url": "/css/chunk-vendors.a0c60aa6.css"
+  },
+  {
+    "revision": "ec885583295e5abd38e107ea9ac4eb4e",
+    "url": "/img/nyc-city-hall-station.ec885583.jpg"
+  },
+  {
+    "revision": "5333754f3dbddd51b4d9e9fc71c9e482",
+    "url": "/img/nyc-dynamo.5333754f.jpg"
+  },
+  {
+    "revision": "f9e888c0a13c5e776c422f30302d81c7",
+    "url": "/img/nyc-more-chrysler.f9e888c0.jpg"
+  },
+  {
+    "revision": "e988a217fa84dfd3ec485403605d40cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52f4222be06aada5eaf1",
+    "url": "/js/about.3f64b827.js"
+  },
+  {
+    "revision": "ddfd96833993224d990d",
+    "url": "/js/app.dc6c62d8.js"
+  },
+  {
+    "revision": "9dbc0a19f13ba1a795f7",
+    "url": "/js/chunk-vendors.b584dd8a.js"
+  },
+  {
+    "revision": "3308412c492f126f3607036f34f615d6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
